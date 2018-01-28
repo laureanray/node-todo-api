d5c3ec61ced75b1361739df58e9105200fc71de6
laureanray <laureanray>
2018-01-28 14:16:08 +0800

    Added the get test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,11 +5,20 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done()); 
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done()); 
 });
 
 describe('Post /todos', () =>{
+    // post request handler
     it('Should create a todo', (done) => {
         var text = 'Test todo text';
         
@@ -25,7 +34,7 @@ describe('Post /todos', () =>{
                  return done(err);
              }
 
-             Todo.find().then((todos) => {
+             Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -34,23 +43,38 @@ describe('Post /todos', () =>{
              });
          })
     });
+    // This is for the bad request handler
+    it('Should not create a todo with bad request', (done) => {
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+
+            Todo.find().then((todos) => {
+                
+                expect(todos.length).toBe(2); // since it should not add
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        })
+    });
 });
 
-it('Should not create a todo with bad request', (done) => {
-    request(app)
-     .post('/todos')
-     .send({})
-     .expect(400)
-     .end((err, res) => {
-         if(err){
-             return done(err);
-         }
 
-         Todo.find().then((todos) => {
-             expect(todos.length).toBe(0); // since it should not add
-             done();
-         }).catch((e) => {
-            done(e);
-         });
-     })
+describe('GET /todos', () => {
+    // get handler
+    it('Should get all todos', (done) => {
+        request(app)
+          .get('/todos')
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todos.length).toBe(2);
+          })
+          .end(done); 
+    });
 });
