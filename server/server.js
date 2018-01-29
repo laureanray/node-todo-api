b72aab891b04e090adbaf8467d6ddd9e4b803ada
laureanray <laureanray>
2018-01-30 07:38:38 +0800

    Deployment to Heroku * ENV Set

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+require('./config/config');
+
 const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-
-let port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -51,38 +53,40 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-  // get the id
- let id = req.params.id;
+  var id = req.params.id;
 
- if(!ObjectID.isValid(id)){
-   return res.status(404).send();
- }
- Todo.findByIdAndRemove(id).then((todo) => {
-  if(!todo){
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
-  } 
-  res.send({todo});
- }).catch((e) => res.status(400).send());
+  }
 
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
-  if (!ObjectID.isValid(id)){
-  return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
 
-  if (_.isBoolean(body.completed) && body.completed){
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
-  } else{
+  } else {
     body.completed = false;
     body.completedAt = null;
   }
-  
-  Todo.findByIdAndUpdate(id, {$set: body}, {$new: true}).then((todo) => {
-    if(!todo){
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
       return res.status(404).send();
     }
 
@@ -92,9 +96,9 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
+  
